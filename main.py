cd0e14446dbc2803a7f3aecd7b0cf23511ba0b1d
Gprinziv <gprinziv@gmail.com>
2023-12-03 15:06:01 +0900

    Day 3 in the bag!

--- main.py
+++ main.py
@@ -1,29 +1,56 @@
-import re
 import math
 
+#Returns all numbers adjacent to an "Engine Part" as a string
+#Searchers the row above, of, and below a part.
+def findNumbers(parts, j, i):
+    for row in [j-1, j, j+1]:
+        if parts[row][i].isnumeric():
+            start, end = i, i+1
+            while start > 0 and parts[row][start-1].isnumeric():
+                start -= 1
+            while end <= len(parts[row]) and parts[row][end].isnumeric():
+                end += 1
+            yield int(parts[row][start:end])
+        else:
+            if parts[row][i-1].isnumeric():
+                start = i
+                while start > 0 and parts[row][start-1].isnumeric():
+                    start -= 1
+                yield int(parts[row][start:i])
+            if parts[row][i+1].isnumeric():
+                end = i+2
+                while end < len(parts[row]) and parts[row][end].isnumeric():
+                    end += 1
+                yield int(parts[row][i+1:end])
+
+
 def p1():
-    limits = {"red": 12, "green": 13, "blue": 14}
     with open("input") as f:
-        games = [re.split("[:;,] ", game) for game in f.read().splitlines()]
+        parts = f.read().splitlines()
 
-    total = 0
-    for game in games:
-        if all(int(draw.split()[0]) <= limits[draw.split()[1]] for game in games for draw in game[1:]):
-            total += int(game[0].split()[1]) 
-    print(total)
+    numbers = []
+    misses = "0123456789."
+    for j in range(len(parts)):
+        for i in range(len(parts[j])):
+            if parts[j][i] not in misses:
+                numbers += findNumbers(parts, j, i)
 
+    print(sum(numbers))
+
+#just need to look forthe gear symbol, the fndNumbers algo will work just fine, and then we can filter only those with two numbers 
 def p2():
     with open("input") as f:
-        games = [re.split("[:;,] ", game)[1:] for game in f.read().splitlines()]
+        parts = f.read().splitlines()
 
-    total = 0
-    for game in games:
-        fewest = {"red": 0, "green": 0, "blue": 0}
-        for draw in game:
-            c = draw.split()
-            fewest[c[1]] = max([fewest[c[1]], int(c[0])])
-        total += math.prod(val for val in fewest.values())
-    print(total)
+    numbers = 0
+    for j in range(len(parts)):
+        for i in range(len(parts[j])):
+            if parts[j][i] == "*":
+                gear = list(findNumbers(parts, j, i))
+                if len(gear) == 2:
+                    numbers += math.prod(gear)
+
+    print(numbers)
 
 p1()
 p2()
