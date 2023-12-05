0130f83a6e6e84c6d84ddf6c2af4ebcdb2f6592b
Gprinziv <gprinziv@gmail.com>
2023-12-05 14:52:58 +0900

    Day 5 some decent progress

--- main.py
+++ main.py
@@ -1,25 +1,24 @@
 import re
-
+#First thought is to pass each seed in. For each step:
+#Find the nearest map point that doesn't go over the origin number (Index 1), find the difference, and add that to the to the Destination number (Index 0) 
 def main():
-    with open("input") as f:
-        cards = f.read().splitlines()
-    #PART 1
-    total = 0
-    #PART 2
-    instances = [0] * len(cards)
+    with open("test") as f:
+        seeds, *maps = f.read().split("\n\n")
+    maps = [re.findall("[0-9]+", map) for map in maps]
+    
+    #Do each seed completely one step at a time.
+    for map in maps:
+        print(map[1::3])
+        #Once the seeds are tep-converted, save them and start the next step
+        #newSeeds = []
+        for seed in seeds:
+            pass
+            #Find the Origin closest to but not over ours. If we fail to find, it meanns the conversion has no change.
+            #map[1::3] is the slice we want?
+            #calculate the difference between the original and "seed", if it's greated than the Range, ignore.
+        #seeds = newSeeds
 
-    for card in range(len(cards)):
-        raw = re.findall("[0-9]+|\|", cards[card])
-        pipe = raw.index("|")
-        wins = sum(i in raw[pipe+1:] for i in raw[1:pipe])
-        #PART 1
-        total += int(2 ** (wins - 1))
-        #PART 2
-        instances[card] += 1
-        for j in range(1, wins+1):
-            if card+j < len(instances):
-                instances[card+j] += instances[card]
-    print(total)
-    print(sum(instances))
+    print(seeds)
+    print(maps)
     
 main()
