3d77f20474d9a0e111eb3637b1c83b094adfd986
Giovanni Prinzivalli <Gprinziv@gmail.com>
2023-12-07 22:02:54 +0900

    Day 7 almost done! Bug quashing

--- main.py
+++ main.py
@@ -1,54 +1,118 @@
 import re
-#First thought is to pass each seed in. For each step:
-#Find the nearest map point that doesn't go over the origin number (Index 0), find the difference, and add that to the to the Destination number (Index 1) 
-def main():
-    with open("test") as f:
-        seeds, *maps = f.read().split("\n\n")
-    seeds = [int(seed) for seed in seeds.split()[1:]]
 
-    #This nightmare code block pre-sorts the instructions for each map by origin number to cheese the logic later.
-    for i, mp in enumerate(maps):
-        coords = [m.split() for m in mp.split("\n")[1:]]
-        newCoords = []
-        for coord in coords:
-            newCoords.append([int(coord[1]), int(coord[0]), int(coord[2])])
-        maps[i] = [x for coord in sorted(newCoords) for x in coord]
-        
-    #Do each seed completely one step at a time.
-    for mp in maps:
-        print(f"Our Origin Points are {mp[::3]}")
-        newSeeds = []
-        
-        for seed in seeds:
-            print(f"Our seed is {seed}")
-            
-            #If the "seed" is before any of the modified origin points, it remains unmodified. 
-            if seed < mp[0]:
-                print(f"Seed {seed} is less than min {mp[0]}")
-                newSeeds.append(seed)
-            #If it's beyond the end of the modification table, it's also unmodified.
-            elif seed >= mp[-3]:
-                if mp[-3] + mp[-1] > seed:
-                    print(f"Seed {seed} is in range of {mp[-3]} (Range: {mp[-1]})")
-                    print(f"New seed value is: {mp[-2] - mp[-3] + seed}")
-                    newSeeds.append(mp[-2] - mp[-3] + seed)
-                else:
-                    print(f"Seed {seed} greater than max {mp[-3]} (Range: {mp[-1]})")
-                    newSeeds.append(seed)
-            else:
-                for i in range(3, len(mp), 3):
-                    if mp[i] > seed:
-                        print(f"Seed {seed} is between {mp[i-3]} and {mp[i]}")
-                        #then we need to look at i-3 and see if it's in range
-                        if mp[i-3] + mp[i-1] > seed:
-                            print(f"Seed in range of {mp[i-3]} (Range:{mp[i-1]})")
-                            print(f"New seed value is: {mp[i-2] - mp[i-3] + seed}")
-                            newSeeds.append(mp[i-2] - mp[i-3] + seed)
-                        else:
-                            print(f"Seed out of range of {mp[i-3]} (Range:{mp[i-1]})")
-                            newSeeds.append(seed)
-                        break
-        print(newSeeds)
-        print()
-        seeds = newSeeds
-main()
+def p1():
+  with open("input") as f:
+    text = f.read()
+    text = re.sub("J", "W", text)
+    text = re.sub("Q", "X", text)
+    text = re.sub("K", "Y", text)
+    text = re.sub("A", "Z", text)
+    bids = {}
+    hands = []
+    for hand in text.splitlines():
+      cards, *bid = hand.split()
+      hands.append(cards)
+      bids[cards] = int(bid[0])
+
+  five, four, full, three, two, one, high = [], [], [], [], [], [], []
+
+  for hand in hands:
+    pairFlag = False
+    tripFlag = False
+    if len(set(hand)) == 5:
+      high.append(hand)
+    else:
+      for card in set(hand):
+        if hand.count(card) == 5:
+          five.append(hand)
+          break
+        elif hand.count(card) == 4:
+          four.append(hand)
+          break
+        elif hand.count(card) == 3:
+          tripFlag = True
+          if pairFlag == True:
+            full.append(hand)
+            break
+        elif hand.count(card) == 2:
+          if pairFlag == True:
+            pairFlag = False
+            two.append(hand)
+            break
+          else:
+            pairFlag = True
+
+            if tripFlag == True:
+              full.append(hand)
+              break
+      if tripFlag == True and pairFlag == False:
+        three.append(hand)
+      elif pairFlag == True and tripFlag == False:
+        one.append(hand)
+
+  ranks = sorted(high) + sorted(one) + sorted(two) + sorted(three)\
+      + sorted(full) + sorted(four) + sorted(five)
+
+  print(ranks)
+
+  total = 0
+  for i, hand in enumerate(ranks):
+    total += (i+1) * bids[hand]
+  print(total)
+
+def p2():
+  with open("input") as f:
+    text = f.read()
+    text = re.sub("J", "0", text)
+    text = re.sub("Q", "X", text)
+    text = re.sub("K", "Y", text)
+    text = re.sub("A", "Z", text)
+    bids = {}
+    hands = []
+    for hand in text.splitlines():
+      cards, *bid = hand.split()
+      hands.append(cards)
+      bids[cards] = int(bid[0])
+
+  five, four, full, three, two, one, high = [], [], [], [], [], [], []
+
+  for hand in hands:
+    cards = set(hand)
+    if len(cards) == 5:
+      high.append(hand)
+    else:
+      cards.discard("0")
+      if len(cards) == 1: #Five of a kind!
+        five.append(hand)
+      elif len(cards) == 4: #We know no jokers, so one pair.
+        one.append(hand)
+      elif len(cards) == 2: #If the count of one of the cards is one, it's a four of a kind. Otherwise it's Full House
+        isFull = True
+        for card in cards:
+          if hand.count(card) == 1:
+            isFull = False
+            four.append(hand)
+            break
+        if isFull:
+          full.append(hand)
+      else: #Two Pair or Three of a Kind. The only valid two pair has no jokers.
+        numOfOnes = sum(hand.count(card) == 1 for card in cards)
+        if numOfOnes == 1:
+          two.append(hand)
+        else:
+          three.append(hand)
+
+  ranks = sorted(high) + sorted(one) + sorted(two) + sorted(three)\
+      + sorted(full) + sorted(four) + sorted(five)
+
+  print(sorted(five))
+
+
+  total = 0
+  for i, hand in enumerate(ranks):
+    total += (i+1) * bids[hand]
+    #print(f"hand {i}: {hand}")
+  print(total)
+
+#p1()
+p2()
