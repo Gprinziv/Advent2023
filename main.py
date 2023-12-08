4836962bdbc97c64c9e5ffb3ec17942d765fb23d
Giovanni Prinzivalli <Gprinziv@gmail.com>
2023-12-08 21:16:32 +0900

    Optimizing part 1 for kicks. Day 7 all done!

--- main.py
+++ main.py
@@ -17,47 +17,22 @@ def p1():
   five, four, full, three, two, one, high = [], [], [], [], [], [], []
 
   for hand in hands:
-    pairFlag = False
-    tripFlag = False
-    if len(set(hand)) == 5:
-      high.append(hand)
-    else:
-      for card in set(hand):
-        if hand.count(card) == 5:
-          five.append(hand)
-          break
-        elif hand.count(card) == 4:
-          four.append(hand)
-          break
-        elif hand.count(card) == 3:
-          tripFlag = True
-          if pairFlag == True:
-            full.append(hand)
-            break
-        elif hand.count(card) == 2:
-          if pairFlag == True:
-            pairFlag = False
-            two.append(hand)
-            break
-          else:
-            pairFlag = True
+    cards = set(hand)
 
-            if tripFlag == True:
-              full.append(hand)
-              break
-      if tripFlag == True and pairFlag == False:
-        three.append(hand)
-      elif pairFlag == True and tripFlag == False:
-        one.append(hand)
+    if len(cards) == 5: high.append(hand)
+    elif len(cards) == 4: one.append(hand)
+    elif len(cards) == 3:
+      if any(hand.count(card) == 3 for card in cards): three.append(hand)
+      else: two.append(hand)
+    elif len(cards) == 2:
+      if any(hand.count(card) == 1 for card in cards): four.append(hand)
+      else: full.append(hand)
+    else: five.append(hand)
 
   ranks = sorted(high) + sorted(one) + sorted(two) + sorted(three)\
       + sorted(full) + sorted(four) + sorted(five)
 
-  print(ranks)
-
-  total = 0
-  for i, hand in enumerate(ranks):
-    total += (i+1) * bids[hand]
+  total = sum((i+1) * bids[hand] for i, hand in enumerate(ranks))
   print(total)
 
 def p2():
@@ -83,35 +58,25 @@ def p2():
     cards.discard("0")
 
     #Jokers have been discarded, so this is high card.
-    if len(cards) == 5:
-      high.append(hand)  
+    if len(cards) == 5: high.append(hand)  
     #Regardless of the 5th card, only one pair is possible (ABCDx)  
-    elif len(cards) == 4:
-      one.append(hand)
+    elif len(cards) == 4: one.append(hand)
     #If the jokers plus any card reach 3, it's a three of a kind, otherwise pair.
     elif len(cards) == 3:
-      if any([hand.count(card) + jokers == 3 for card in cards]):
-        three.append(hand)
-      else:
-        two.append(hand)
+      if any([hand.count(card) + jokers == 3 for card in cards]): three.append(hand)
+      else: two.append(hand)
     #With two unique cards, the options are a full house (AABBx) or four of a kind
     elif len(cards) == 2:
-      if any([hand.count(card) == 1 for card in cards]):
-        four.append(hand)
-      else:
-        full.append(hand)
+      if any([hand.count(card) == 1 for card in cards]): four.append(hand)
+      else: full.append(hand)
     #If only one card appears, it must be 5 of a kind!
-    else:
-        five.append(hand)
+    else: five.append(hand)
 
   ranks = sorted(high) + sorted(one) + sorted(two) + sorted(three)\
       + sorted(full) + sorted(four) + sorted(five)
 
-  total = 0
-  for i, hand in enumerate(ranks):
-    total += (i+1) * bids[hand]
-    print(f"hand {i+1}: {hand}")
+  total = sum((i+1) * bids[hand] for i, hand in enumerate(ranks))
   print(total)
 
-#p1()
+p1()
 p2()
